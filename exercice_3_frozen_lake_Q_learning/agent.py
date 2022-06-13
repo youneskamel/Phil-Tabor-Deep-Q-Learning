fbaac2404a7ecf7654c6eb25282cf79ca7a91203
“youneskamel” <mia.san.younes@gmail.com>
2022-06-13 13:27:32 -0400

    refactoring and comments

--- exercice_3_frozen_lake_Q_learning/agent.py
+++ exercice_3_frozen_lake_Q_learning/agent.py
@@ -25,12 +25,10 @@ class Agent :
     # Arguments : Reward is the reward received from the chosen action
     # s_prime is the new state after the action 
     def update_Q(self, reward, s_prime, action):
-        #print("reward :", reward)
         current_q = self.Q[self.s][action]
         # Choose the highest reward action knowing we're in s_prime
         max_next_q = max(self.Q[s_prime].values())
         self.Q[self.s][action] = current_q + self.alpha*(reward + self.gamma*(max_next_q - current_q))
-        #print(self.Q.values())
 
     # Function used to update the state in the agent class
     def update_state(self, s_prime):
@@ -39,12 +37,12 @@ class Agent :
     # Function used by the agent to choose next action
     def action(self) :
         eps = self.epsilon_max
-        print(eps)
+        # define an optimalaction and a random action
         optimal_action = max(self.Q[self.s], key=self.Q[self.s].get)
         random_action = random.randint(0, 3)
-        sampleList = [optimal_action, random_action]
-        action = random.choices(sampleList, weights=(1-eps,eps), k=2)
-        action = action[0]
+        # choose between the optimal and random actions, weighed by epsilon
+        possible_actions = [optimal_action, random_action]
+        action = random.choices(possible_actions, weights=(1-eps,eps), k=2)[0]
         if self.epsilon_max > self.epsilon_min :
             self.epsilon_max -= 1/4600000
         return action
