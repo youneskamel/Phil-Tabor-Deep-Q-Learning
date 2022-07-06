47e5c22d26975ee00f7ac5412eb84b1fa29dc15e
“youneskamel” <mia.san.younes@gmail.com>
2022-07-06 17:19:45 -0400

    agent and q_network should be ready, may have bugs

--- exercice_4_frozen_lake_naive_deep_Q/agent.py
+++ exercice_4_frozen_lake_naive_deep_Q/agent.py
@@ -1,34 +1,64 @@
 from q_network import Qnetwork
+import torch as T
+
+# This is the class we will import in the environement to let it learn
+#Arguments : 
 class Agent():
-    def __init__(self, lr, gamma, n_actions, n_states, eps_start, eps_end, eps_dec):
+    def __init__(self, lr, n_actions, n_states, input_dims, gamma=0.99, epsilon=1, eps_min=0.01, eps_dec=0.005):
         self.lr = lr
         self.gamma = gamma
         self.n_actions = n_actions
         self.n_states = n_states
-        self.epsilon = eps_start
-        self.eps_min = eps_end
+        self.epsilon = epsilon
+        self.eps_min = eps_min
         self.eps_dec = eps_dec
-        self.neural_net = NeuralNet(0.001,self.n_actions,)
+        self.input_dims = input_dims
+        self.q_network = Qnetwork(self.lr, self.n_actions, self.input_dims)
 
-    #roughly right
+    # Function that the agent uses to choose the next action
+    # Argument : state is the current state of the environement 
     def choose_action(self, state):
-        state = state #convert to pytorch tensor
-        optimal_action = self.neural_net.forward(state)
+        state = T.tensor(state, dtype=T.float).to(self.q_network.device)
+        optimal_action = T.argmax(self.q_network.forward(state)).item()
         random_action = np.random.choice(3,1)[0]
         next_action = 0
         if np.random.random() < self.epsilon:
             next_action = random_action
         else:
             next_action = optimal_action
-
         return next_action
-         
+    
+    # Decrease epsilon to make the algorithm more greedy
     def decrease_epsilon(self):
         self.epsilon = self.epsilon*self.eps_dec if self.epsilon>self.eps_min\
                        else self.eps_min
     
-    def learn(self, reward, obs, action):
-        data = [reward, obs, action]
-        self.neural_net.learn(data, labels)
-    
+    def learn(self, reward, state, next_state, action):
+        # Reset the gradients because we will compute them again with backward()
+        self.q_network.optimizer.zero_grad()
+
+        # Transform arguments into tensors for PyTorch
+        state = T.tensor(state).to(self.q_network.device)
+        next_state = T.tensor(next_state).to(self.q_network.device)
+        action = T.tensor(action).to(self.q_network.device)
+        reward = T.tensor(reward).to(self.q_network.device)
+        
+        # You need the Q value predicted for the action you just took
+        # So you pass the action as the index to the returned tensor from forward()
+        q_pred = self.q_network.forward(state)[action]
+
+        q_next = self.q_network.forward(next_state).max()
+
+        # Why don't we subtract q_pred here like in the Bellman equation ?
+        # I think it's because we are not incrementing Q but updating the weights of the network to predict it
+        # The Bellman equation q <- q + alpha*(reward + gamma*q_next -q) is an iterative incrementation equation
+        # Here we don't want to increment q but to precit it's maximum value which is teh reward + the max value of q_next 
+        q_bootstrap = reward + self.gamma*q_next - q_pred
+
+
+        loss = self.q_network.loss(q_bootstrap, q_pred).to(self.Q.device)
+        loss.backward()
+        self.q_network.optimizer.step()
+        self.decrease_epsilon()
+
     
--- exercice_4_frozen_lake_naive_deep_Q/q_network.py
+++ exercice_4_frozen_lake_naive_deep_Q/q_network.py
@@ -1,22 +1,25 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch as T
 
+# The neural network that replaces our Q-value table
+# It takes the state as input data and output the state-action value (Q)
+# Arguments : the learning rate, the number of possible actions, the dimmensions of the input
 class Qnetwork(nn.Module):
     def __init__(self, lr, n_classes, input_dims):
         super(Qnetwork, self).__init__()
 
+        # Not sure why input dims has a *, maybe unpacking a variadic argument ?
         self.fc1 = nn.Linear(*input_dims, 128)
         self.fc2 = nn.Linear(128, n_classes)
 
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.loss = nn.MSELoss()
-		#use GPU if available, else CPU
+		# use GPU if available, else CPU
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
 
-	#forward propagation
+	# forward propagation
     def forward(self, state):
         layer1 = F.relu(self.fc1(state))
         layer2 = self.fc2(layer1)
