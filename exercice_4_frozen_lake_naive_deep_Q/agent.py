25c57284142d7107af590fe1314a6406bd31567d
“youneskamel” <mia.san.younes@gmail.com>
2022-07-05 22:34:37 -0400

    ok

--- exercice_4_frozen_lake_naive_deep_Q/agent.py
+++ exercice_4_frozen_lake_naive_deep_Q/agent.py
@@ -1,4 +1,4 @@
-from neural_net import NeuralNet
+from q_network import Qnetwork
 class Agent():
     def __init__(self, lr, gamma, n_actions, n_states, eps_start, eps_end, eps_dec):
         self.lr = lr
@@ -8,27 +8,27 @@ class Agent():
         self.epsilon = eps_start
         self.eps_min = eps_end
         self.eps_dec = eps_dec
-        self.neural_net = NeuralNet()
+        self.neural_net = NeuralNet(0.001,self.n_actions,)
 
-    
-    def choose_action(self, reward, obs, action):
-        data = [reward, obs, action]
-        optimal_action = self.neural_net.forward(data)
+    #roughly right
+    def choose_action(self, state):
+        state = state #convert to pytorch tensor
+        optimal_action = self.neural_net.forward(state)
         random_action = np.random.choice(3,1)[0]
         next_action = 0
         if np.random.random() < self.epsilon:
             next_action = random_action
         else:
             next_action = optimal_action
-        if self.epsilon > self.epsilon_min :
-            self.epsilon = self.epsilon*0.9999995
+
         return next_action
          
-    
-    def learn(self, reward, obs, action):
-        self.neural_net.learn(data, labels)
-    
-    
     def decrease_epsilon(self):
         self.epsilon = self.epsilon*self.eps_dec if self.epsilon>self.eps_min\
                        else self.eps_min
+    
+    def learn(self, reward, obs, action):
+        data = [reward, obs, action]
+        self.neural_net.learn(data, labels)
+    
+    
---
+++ exercice_4_frozen_lake_naive_deep_Q/q_network.py
@@ -0,0 +1,26 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch as T
+
+class Qnetwork(nn.Module):
+    def __init__(self, lr, n_classes, input_dims):
+        super(Qnetwork, self).__init__()
+
+        self.fc1 = nn.Linear(*input_dims, 128)
+        self.fc2 = nn.Linear(128, n_classes)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+		#use GPU if available, else CPU
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+	#forward propagation
+    def forward(self, state):
+        layer1 = F.relu(self.fc1(state))
+        layer2 = self.fc2(layer1)
+
+        return layer2
+
+  
