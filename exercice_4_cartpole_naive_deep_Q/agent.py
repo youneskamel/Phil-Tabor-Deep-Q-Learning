302942023d94600ceae56e6f3a9223aeb222ff90
“youneskamel” <mia.san.younes@gmail.com>
2022-07-07 11:32:55 -0400

    epsilon decreases, but performance decreases

--- exercice_4_cartpole_naive_deep_Q/agent.py
+++ exercice_4_cartpole_naive_deep_Q/agent.py
@@ -5,7 +5,7 @@ import torch as T
 # This is the class we will import in the environement to let it learn
 #Arguments : 
 class Agent():
-    def __init__(self, lr, n_actions, input_dims, gamma=0.99, epsilon=1, eps_min=0.01, eps_dec=0.005):
+    def __init__(self, lr, n_actions, input_dims, gamma=0.99, epsilon=1.0, eps_min=0.01, eps_dec=1e-5):
         self.lr = lr
         self.gamma = gamma
         self.n_actions = n_actions
@@ -23,7 +23,6 @@ class Agent():
         state = T.tensor(state, dtype=T.float).to(self.q_network.device)
         optimal_action = T.argmax(self.q_network.forward(state)).item()
         random_action = np.random.choice(self.action_space)
-        #next_action = 0
         if np.random.random() < self.epsilon:
             next_action = random_action
         else:
@@ -32,9 +31,9 @@ class Agent():
     
     # Decrease epsilon to make the algorithm more greedy
     def decrease_epsilon(self):
-        self.epsilon = self.epsilon*self.eps_dec if self.epsilon>self.eps_min\
-                       else self.eps_min
-    
+        if self.epsilon > self.eps_min :
+            self.epsilon = self.epsilon-self.eps_dec
+
     def learn(self, reward, state, next_state, action):
         # Reset the gradients because we will compute them again with backward()
         self.q_network.optimizer.zero_grad()
@@ -56,7 +55,6 @@ class Agent():
         # The Bellman equation q <- q + alpha*(reward + gamma*q_next -q) is an iterative incrementation equation
         # Here we don't want to increment q but to precit it's maximum value which is teh reward + the max value of q_next 
         q_bootstrap = reward + self.gamma*q_next - q_pred
-
 
         loss = self.q_network.loss(q_bootstrap, q_pred).to(self.q_network.device)
         loss.backward()
